26a60ad53dafe40fdd778a453b7917cc878d9acb
A.Toraby <128970437+COPOL2850@users.noreply.github.com>
2023-10-24 20:58:13 +0330

    Delete src/permanents/Api/Api keys directory

--- src/permanents/Api/Api keys/search Api keys.js
+++
@@ -1,6 +0,0 @@
-export const searchApiKeys = () => {
-  return  ({
-    "X-RapidAPI-Key": "3d267e7a9bmsh3f6f85c02501cacp1270fejsn6cfdfa0075aa",
-    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-  });
-};
